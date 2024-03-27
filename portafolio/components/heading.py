4ba36136c7e4d6669a81d8cd5bf39af1d7b7e993
fabimass <massotto.fabian@gmail.com>
2024-03-27 17:46:42 -0300

    fix responsive headers

--- portafolio/components/heading.py
+++ portafolio/components/heading.py
@@ -6,5 +6,6 @@ def heading(text: str, h1=False) -> rx.Component:
     return rx.heading(
         text,
         as_="h1" if h1 else "h2",
-        size=Size.BIG.value if h1 else Size.DEFAULT.value
+        size=Size.BIG.value if h1 else Size.DEFAULT.value,
+        display=["flex", "flex", "flex", "flex", "flex"]
     )
